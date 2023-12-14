e834d61114435f36de819f3fcf642e84b32ed2ae
jigya <153490195+Jigyasha15@users.noreply.github.com>
2023-12-14 08:22:16 +0530

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+void main()
+{
+    int x=0;
+    while(x<101)
+    {
+        printf("%dd/n",x);
+        x++;
+    }
+}
